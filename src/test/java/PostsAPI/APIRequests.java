989b419237132d866606174a25ba7d6599b336e2
abhishek-kolapkar <abhisheknkolapkar@gmail.com>
2024-11-11 07:12:53 +0530

    API request for create new post with data

--- src/test/java/PostsAPI/APIRequests.java
+++ src/test/java/PostsAPI/APIRequests.java
@@ -1,6 +1,8 @@
 package PostsAPI;
 
 import static io.restassured.RestAssured.*;
+
+import org.json.JSONObject;
 import org.testng.annotations.Test;
 
 public class APIRequests {
@@ -9,10 +11,29 @@ public class APIRequests {
     given()
 
         .when()
-            .get("https://jsonplaceholder.typicode.com/posts")
-          
+        .get("https://jsonplaceholder.typicode.com/posts")
+
         .then()
-            .statusCode(200)
+        .statusCode(200)
+        .log().all();
+  }
+  
+  @Test
+  void createPost() {
+    JSONObject data = new JSONObject();
+    data.put("title", "New Post");
+    data.put("body", "This is the body of the new post");
+    data.put("userId", 1);
+
+    given()
+        .contentType("application/json")
+        .body(data)
+
+        .when()
+            .post("https://jsonplaceholder.typicode.com/posts")
+            
+        .then()
+            .statusCode(201)
             .log().all();
   }
 }
